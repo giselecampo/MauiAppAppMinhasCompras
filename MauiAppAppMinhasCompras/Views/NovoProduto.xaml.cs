14ff303712a7498faefc8fb277ae22e8d26cd5b0
gisele <gisele.ap.campo@gmail.com>
2025-09-07 11:00:07 -0300

    Add funcionalidades

--- MauiAppAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,25 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+            await DisplayAlert("Sucesso!", "Registro Inserido", "Ok");
+
+
+		} catch(Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "Ok");
+		}
+    }
 }
